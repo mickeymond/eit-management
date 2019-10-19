56fb266a0447c4475c37815d0116ebd171dad148
Michael Hammond <mickeymond@gmail.com>
2019-10-19 21:25:10 +0000

    Beginnig Bulk Delete

---
+++ client/js/home.js
@@ -0,0 +1,58 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var'
+
+import EITs from '../../lib/collections/eits';
+
+Template.home.onCreated(function bodyOnCreated() {
+  this.isAllChecked = new ReactiveVar(false);
+  this.checkedEITs = new ReactiveVar([]);
+});
+
+// Template Helpers
+Template.home.helpers({
+  eits() {
+    return EITs.find({});
+  },
+
+  isAllChecked() {
+    const instance = Template.instance();
+    return instance.isAllChecked.get();
+  }
+});
+
+Template.home.events({
+  "click .delete-eit": function(e) {
+    // Prevent Default Action
+    e.preventDefault();
+
+    // Call delete method
+    Meteor.call('eits.delete', this._id);
+  },
+
+  "change #all": function(e, instance) {
+    // Prevent Default Action
+    e.preventDefault();
+
+    if (e.target.checked) {
+      instance.isAllChecked.set(true);
+    } else {
+      instance.isAllChecked.set(false);
+    }
+  },
+
+  "change .eit-check": function(e, instance) {
+    // Prevent Default Action
+    e.preventDefault();
+
+    if (e.target.checked) {
+      const oldEitIds = instance.checkedEITs.get();
+      instance.checkedEITs.set([...oldEitIds, e.target.id]);
+    } else {
+      const oldEitIds = instance.checkedEITs.get();
+      instance.checkedEITs.set(oldEitIds.filter(each => each !== e.target.id));
+    }
+
+    console.log(instance.checkedEITs.get());
+  }
+});
